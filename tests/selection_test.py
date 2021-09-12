816a4131d8d2e0de80b7aa648a7c60ce586b4cd4
Gustavo Viegas <webviegas@gmail.com>
2021-09-12 01:48:03 -0300

    Atualiza testes

--- tests/selection_test.py
+++ tests/selection_test.py
@@ -4,24 +4,26 @@ from tests import *
 from unittest import TestCase
 
 import pytest
-import random
-
-# def generateRandomValue():
-#     return [random.uniform(0, 10) for _i in range(2)]
-
-# random_population = [Individual(generateRandomValue()) for _i in range(10)]
 
 
 class TestGA(TestCase):
     def test_tournament_returns_empty_if_population_empty(self):
         # ind = Individual()
         pop = []
-        tourn_result = tournament([], 5)
+        tourn_result = tournament([], 1, 1)
         self.assertEqual(tourn_result, [])
 
     def test_tournament_raises_if_tourn_size_greater_than_pop_size(self):
         # ind = Individual()
         pop = [Individual(), Individual(), Individual()]
         with self.assertRaises(ValueError) as e:
-            tournament(pop, 4)
+            tournament(pop, 1, 4)
             self.assertEqual(str(e), "Sample larger than population or is negative")
+
+    def test_tournament_should_return_best_fit_individual(self):
+        pop = [Individual(), Individual()]
+        pop[0].fitness = 50
+        pop[1].fitness = 1
+
+        tourn_result = tournament(pop, 1, 2)
+        self.assertEqual(tourn_result[0], pop[0])
