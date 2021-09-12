71a2b35a7e99292c285c92522711471fce86be9e
Mario Sergio <marionbxcz@gmail.com>
2021-09-12 01:45:44 -0300

    feat: create roulette function

--- meta_tp2/selection.py
+++ meta_tp2/selection.py
@@ -1,9 +1,9 @@
 import random
 
-def tournament(population, tournamentSize):
+def tournament(population, selectionSize, tournamentSize):
   tournamentResult = []
 
-  for i in range(len(population)):
+  for i in range(selectionSize):
     randomSelection = random.sample(population, k=tournamentSize)
 
     best = randomSelection[0]
@@ -13,3 +13,22 @@ def tournament(population, tournamentSize):
     tournamentResult.append(best)
 
   return tournamentResult
+
+def roulette(population, selectionSize):
+    populationFitnessSum = 0
+    for individual in population:
+        populationFitnessSum += individual.fitness
+
+    rouletteResult = []
+
+    for i in range(selectionSize):
+        randomValue = random.uniform(0, populationFitnessSum)
+
+        iterationFitnessSum = 0
+        for individual in population:
+            iterationFitnessSum += individual.fitness
+            if iterationFitnessSum >= randomValue:
+                rouletteResult.append(individual)
+                break
+
+    return rouletteResult
